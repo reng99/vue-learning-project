fbe5496350e95735064eaf546d078df221532baf
reng <1837895991@qq.com>
2016-12-13 16:54:15 +0800

    begin with the less
    this is the the simple project of combining the less to the vue

---
+++ lessTest/src/components/Hello.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <div class="classa">
+      <div class="classb">
+        this is the test content about the less!
+      </div>
+    </div>
+    <hr/>
+    <br/>
+    <div class="reng">
+      <button type="button" name="button" @click="goSuffle">suffle</button>
+      <br/><hr/>
+        <span v-for="item in items">
+            {{item}}
+        </span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+window._ = require('underscore')
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      items:[1,2,3,4,5,6,7,8,9]
+    }
+  },
+  methods:{
+    goSuffle:function(){
+      this.items=_.shuffle(this.items)
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  @import "../../static/less/test.less";
+
+  .classa{
+    width: 300px;
+    height: 100px;
+    background: red;
+    border: 1px solid blue;
+    .classb{
+      width: 200px;
+      height: 50px;
+      line-height: 50px;
+      color: @color;
+      font-size: @fontSize;
+      border: 1px solid @color;
+      overflow: hidden;
+    }
+  }
+</style>
